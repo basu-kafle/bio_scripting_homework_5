8f42861b0d236336fa249287ad3d73006917fc9f
basu-kafle <bzk0055@auburn.edu>
2020-02-25 16:42:41 -0600

    removing more spaces

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -10,23 +10,23 @@ add=$((a+b))
 
 echo Addition of a and b are $add
 
-sub=$((a - b))
+sub=$((a-b))
 echo Subtraction of a and b are $sub
 
-mul=$((a * b))
+mul=$((a*b))
 echo Multiplication of a and b are $mul
 
-div=$((a / b))
+div=$((a/b))
 echo division of a and b are $div
 
-mod=$((a % b))
+mod=$((a%b))
 echo Modulus of a and b are $mod
 
 ((++a))
-echo Increment operator when applied on "a" results into a = $a
+echo Increment operator when applied on "a" results into a=$a
 
 ((--b))
-echo Decrement operator when applied on "b" results into b = $b
+echo Decrement operator when applied on "b" results into b=$b
 
 ((--c))
 echo What was the default value \ of c if its value is now $c\?
