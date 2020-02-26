1e9c8903e234175eec423af6d3da9813b767c8a4
basu-kafle <bzk0055@auburn.edu>
2020-02-25 18:42:59 -0600

    added missing shebang line and comments

--- part_2/sys_info_kafle.sh
+++ part_2/sys_info_kafle.sh
@@ -1,3 +1,10 @@
+#!/bin/bash 
+
+# time and date
 time_date= date "+%c" >>sys_info_kafle.txt
+
+#list of logged-in users
 logged_in_users= who -u >>sys_info_kafle.txt
+
+#system uptime
 system_uptime= uptime >>sys_info_kafle.txt
